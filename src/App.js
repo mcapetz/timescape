bbb6ccf9aafd7575ee02f8ee66f54e6cc0021ae2
mcapetz <pcdcmccc@gmail.com>
2022-07-11 20:16:15 -0700

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React!
+          <div className="text-blue-700">HELLO</div>
         </a>
       </header>
     </div>
